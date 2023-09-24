518c2e004ffac0536d4a4452d4ca4c09ac043d2a
ihsan215 <aliihsantas34@gmail.com>
2023-09-25 01:02:57 +0300

    tested create_project function

--- smart_contracts/test/FundMe.test.js
+++ smart_contracts/test/FundMe.test.js
@@ -14,5 +14,59 @@ contract("FundMe Contract", (accounts) => {
       const acc = await contractInstance.admin();
       assert.equal(acc, admin);
     });
+
+    it("check create project functiın", async () => {
+      // Check function
+      const title = "Example Project 1";
+      const description = "example description";
+      const min_contribution = 1;
+
+      const result = await contractInstance.create_project(
+        title,
+        description,
+        min_contribution,
+        {
+          from: accounts[1],
+        }
+      );
+
+      // Check event
+      assert.equal(
+        result.logs[0].args.id.toNumber(),
+        0,
+        "first project id equal to 0"
+      );
+
+      assert.equal(
+        result.logs[0].args.owner,
+        accounts[1],
+        "project owner check"
+      );
+
+      assert.equal(result.logs[0].args.title, title, "project title check");
+
+      // Check result
+      assert.equal(result.receipt.status, true, "status must be true");
+
+      // Check project count
+      try {
+        for (let i = 0; i < 6; i++) {
+          await contractInstance.create_project(
+            title,
+            description,
+            min_contribution,
+            {
+              from: accounts[1],
+            }
+          );
+        }
+        assert(false);
+      } catch (e) {
+        assert(
+          e.message.indexOf("revert") >= 0,
+          "error message must contain revert"
+        );
+      }
+    });
   });
 });
