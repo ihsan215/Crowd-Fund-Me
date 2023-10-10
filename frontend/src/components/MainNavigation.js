b62e5634fef43ba092b74f5923b5139035cc196c
ihsan215 <aliihsantas34@gmail.com>
2023-10-10 15:30:16 +0300

    edited navbar

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,34 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../style/components/MainNavigation.css";
 import WalletConnectBtn from "../UI/WalletConnectBtn.js";
+
+import "../style/components/MainNavigation.css";
 
 function MainNavigation() {
   return (
     <React.Fragment>
-      <nav className="navbar sticky-top  navbar-expand-lg myNav">
-        <Link class="navbar-brand" to="/">
-          <p className="logo-title"> &lt;&lt; CrodFundMe &gt;&gt;</p>
-        </Link>
-        <button
-          class="btn navbar-toggler me-5"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#collapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <nav className="main-nav d-flex-c">
+        <div className="logo">
+          <h1>Logo</h1>
+        </div>
 
-        <div className="navbar-collapse collaps" id="collapse">
-          <hr></hr>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 me-5 ">
-            <li className="nav-item  m-auto">
-              <WalletConnectBtn />
-            </li>
+        <div className="menu">
+          <ul className="menu-list d-flex-c">
+            <li className="menu-item">Explore</li>
+            <li className="menu-item">Projects</li>
+            <li className="menu-item">Contact Us</li>
+            <li className="menu-item">Contact Us</li>
+            <li className="menu-item">Contact Us</li>
           </ul>
         </div>
+
+        <div className="search-menu">
+          <input className="search" />
+        </div>
+
+        <div className="connect-wallet-btn">Connect Btn</div>
       </nav>
     </React.Fragment>
   );
