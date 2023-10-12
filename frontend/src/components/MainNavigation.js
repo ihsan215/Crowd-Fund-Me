e561314a174834f876d8b916a2719edfa3763c76
ihsan215 <aliihsantas34@gmail.com>
2023-10-12 12:18:19 +0300

    edited responsive navbar

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import WalletConnectBtn from "../UI/WalletConnectBtn.js";
-
 import "../style/components/MainNavigation.css";
-
 import logoIcon from "../style/img/logo.svg";
 
 function MainNavigation() {
+  const [isMenuDisplay, setIsMenuDisplay] = useState(false);
+
+  const menuBtn = () => {
+    setIsMenuDisplay(!isMenuDisplay);
+  };
+
   return (
     <React.Fragment>
       <nav className="main-nav d-flex-c">
@@ -16,26 +20,35 @@ function MainNavigation() {
           </Link>
         </div>
 
-        <div className="menu d-flex-c">
+        <div className={`menu d-flex-c ${isMenuDisplay ? "" : "hide"}`}>
           <ul className="menu-list d-flex-c">
             <li className="menu-item">Explore</li>
             <li className="menu-item">Projects</li>
             <li className="menu-item">Contact Us</li>
+            <li>
+              {" "}
+              <div className="search-menu">
+                <input
+                  className="search"
+                  placeholder="search projects"
+                  type="search"
+                />
+              </div>
+            </li>
+            <li>
+              {" "}
+              <div className="connect-wallet-btn">
+                <WalletConnectBtn />
+              </div>
+            </li>
           </ul>
-
-          <div className="search-menu">
-            <input
-              className="search"
-              placeholder="search projects"
-              type="search"
-            />
-          </div>
-
-          <div className="connect-wallet-btn">
-            <WalletConnectBtn />
-          </div>
         </div>
+
+        <button onClick={menuBtn} className="nav-dropdown-menu">
+          &#9776;
+        </button>
       </nav>
+      <div className={`menu-area ${isMenuDisplay ? "" : "hide"}`}></div>
     </React.Fragment>
   );
 }
