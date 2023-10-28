dfccf2e07dfd1983efbdadbb2dc6e195d8531007
ihsan215 <aliihsantas34@gmail.com>
2023-10-28 15:45:55 +0300

    edited web3walletmodal

--- frontend/src/components/Web3WalletModal.js
+++ frontend/src/components/Web3WalletModal.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Modal from "../UI/Modal";
 
 import Web3Context from "../web3/Web3-context.js";
@@ -7,26 +7,40 @@ import CopyIcon from "../UI/CopyIcon";
 
 import EthIcon from "../style/img/ethereum.png";
 
+import "../style/Web3WalletModal.css";
+
 function Web3WalletModal({ onClose, msg }) {
+  const [isCopied, setIsCopied] = useState(false);
+
   const web3Ctx = useContext(Web3Context);
 
   const clickCopyBoard = () => {
     navigator.clipboard.writeText(web3Ctx.address);
-
-    return <h1>COPYED</h1>;
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
   };
 
   return (
     <React.Fragment>
       <Modal onClose={onClose} msg={msg}>
-        <div>
+        <div className="web3-wallet-modal-area">
           <img src={EthIcon} alt="eth icon" style={{ width: "125px" }} />
-          <p>
-            {walletAddressFormat(web3Ctx.address)}{" "}
-            <span>
-              <CopyIcon onClick={clickCopyBoard} />
-            </span>
-          </p>
+          <div className="wallet-info-area">
+            <p>{web3Ctx.balance} SepoliaETH</p>
+            <p>|</p>
+            <p>
+              {walletAddressFormat(web3Ctx.address)}{" "}
+              <span>
+                <CopyIcon
+                  onClick={clickCopyBoard}
+                  isCopied={isCopied}
+                  className="copy-icon-area"
+                />
+              </span>
+            </p>
+          </div>
         </div>
       </Modal>
     </React.Fragment>
